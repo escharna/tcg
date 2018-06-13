cc967bd0f7f351e5190449d3f44a52231c5c1c81
Eric Scharnak <ericscharnak@erics-mbp.attlocal.net>
2018-06-12 23:10:09 -0400

    Routing

--- app/assets/javascripts/angular/app.module.js
+++ app/assets/javascripts/angular/app.module.js
@@ -4,14 +4,15 @@
   angular.module('application', [
     'ng-token-auth',
     'ui.router',
+    'ngResource',
     'templates',
     'application.homeComponent',
     'application.lobbyComponent',
     'application.usersComponent',
     'application.usersFactory'
   ]).config(function($authProvider) {
-    $authProvider.configure({
-      apiUrl: 'http://localhost:3000'
-    });
+    // $authProvider.configure({
+    //   apiUrl: 'http://localhost:3000'
+    // });
   });
 })();
